4bea91aed5c27eaa3b0c0b2240c9f330b6c86161
Zabihullah Danish <zabihullahdanish01@gmail.com>
2023-01-08 14:44:08 +0430

    signin and signup page included

--- resources/views/about/aboutUs.blade.php
+++ resources/views/about/aboutUs.blade.php
@@ -4,6 +4,8 @@
 @include('partials.aboutUsHero')
 @include('partials.mission')
 @include('partials.vission')
+@include('partials.signIn')
+@include('partials.signUp')
 @include('partials.headQuarters')
 @include('partials.footer')
     
