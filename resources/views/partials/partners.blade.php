ed44cff78d718c74d8bb4c7b006672fe4e7df249
Zabihullah Danish <zabihullahdanish01@gmail.com>
2023-01-12 23:44:15 +0430

    parteners page updated

--- resources/views/partials/partners.blade.php
+++ resources/views/partials/partners.blade.php
@@ -1,28 +1,33 @@
 <!-- paartners start here ======
   ====== -->
-  <div class="container partners-section">
+<div class="container partners-section">
+
     <div class="row align-items-center h-25">
+
         <div class="container rounded">
             <h1 class="text-center mission-typo">Our Partners</h1>
             <div class="slider">
-                <div class="slider-logos tw-max-w-[80%]">
-                <div class="row slider-logos">
-                    <i class="partner "><img class="w-25" src="{{ asset('images/amazon.png') }}" alt=""></i>
-                    <i class="partner "><img class="w-25" src="{{ asset('images/apple.png') }}" alt=""></i>
-                    <i class="partner "><img class="w-25" src="{{ asset('images/google.jpg') }}" alt=""></i>
-                    <i class="partner "><img class="w-25" src="{{ asset('images/microsoft.jpeg') }}" alt=""></i>
+                
+                <div class="slider-logos tw-max-w-[80%] tw-flex tw-gap-3">
+
+                    <div class="slider-logos tw-flex row">
+                        <i class="partner "><img src="{{ asset('images/amazon.png') }}" alt=""></i>
+                        <i class="partner "><img src="{{ asset('images/apple.png') }}" alt=""></i>
+                        <i class="partner "><img src="{{ asset('images/google.jpg') }}" alt=""></i>
+                        <i class="partner "><img src="{{ asset('images/microsoft.jpeg') }}" alt=""></i>
+                        
+                    </div>
 
                 </div>
-                <div class="slider-logos tw-max-w-[80%]">
-                <div class="slider-logos row">
-                    <i class="partner "><img src="{{ asset('images/amazon.png') }}" alt=""></i>
-                    <i class="partner "><img src="{{ asset('images/apple.png') }}" alt=""></i>
-                    <i class="partner "><img src="{{ asset('images/google.jpg') }}" alt=""></i>
-                    <i class="partner "><img src="{{ asset('images/microsoft.jpeg') }}" alt=""></i>
-                </div>
+
+                
+
             </div>
+
         </div>
+
     </div>
+
 
 </div>
   <!-- partners end here =======
