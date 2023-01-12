f9f0c25a3c14a73f333972af326752179d422bfb
Zabihullah Danish <zabihullahdanish01@gmail.com>
2023-01-12 23:53:14 +0430

    font-awesome added

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,68 @@
+<div class="tw-w-[15%] tw-top-0 tw-bottom-0 tw-left-0 tw-fixed tw-bg-[#14213D] tw-text-gray-100">
+    <div class="">
+        <div class="tw-w-[50%] tw-mx-auto tw-shadow-md tw-shadow-indigo-600 tw-m-2 tw-rounded-md tw-border tw-border-indigo-600">
+            <img src="{{ asset('images/codeweekend.png') }}" alt="">
+        </div>
+        <div id="nav-links" class=" tw-flex tw-flex-col tw-bg-[#192745] tw-divide-y tw-divide-indigo-900 tw-mt-4 tw-font-[roboto-bold] tw-text-xl">
+            <a id="dashboard" class="tw-p-2 tw-px-4 hover:tw-text-indigo-400 tw-flex tw-justify-between tw-items-center" href="{{ route('dashboard') }}">
+                <div>
+                    <span><i class="fa fa-home"></i></span>
+                    <span>Dashboard</span>
+                </div>
+                <i class="fa fa-angle-left"></i>
+            </a>
+            <a id="chapters" class="tw-p-2 tw-px-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="{{ route('chapters') }}">
+                <div>
+                    <span><i class="fa fa-book hover:tw-text-indigo-400"></i></span>
+                    <span>Chapters</span>
+                </div>
+                <i class="fa fa-angle-left"></i>
+            </a>
+            <a id="organizers" class="tw-p-2 tw-px-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="{{ route('chapters') }}">
+                <div>
+                    <span><i class="fa fa-user hover:tw-text-indigo-400"></i></span>
+                    <span>Organizers</span>
+                </div>
+                <i class="fa fa-angle-left"></i>
+            </a>
+            <a id="mentors" class="tw-p-2 tw-px-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="{{ route('chapters') }}">
+                <div>
+                    <span><i class="fa fa-users hover:tw-text-indigo-400"></i></span>
+                    <span>Mentors</span>
+                </div>
+                <i class="fa fa-angle-left"></i>
+            </a>
+            <a id="sessions" class="tw-p-2 tw-px-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="{{ route('chapters') }}">
+                <div>
+                    <span><i class="fa fa-phone hover:tw-text-indigo-400"></i></span>
+                    <span>Sessions</span>
+                </div>
+                <i class="fa fa-angle-left"></i>
+            </a>
+            <a id="assignments" class="tw-p-2 tw-px-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="{{ route('chapters') }}">
+                <div>
+                    <span><i class="fa fa-circle hover:tw-text-indigo-400"></i></span>
+                    <span>Assignments</span>
+                </div>
+                <i class="fa fa-angle-left"></i>
+            </a>
+            <a id="cities" class="tw-p-2 tw-px-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="{{ route('chapters') }}">
+                <div>
+                    <span><i class="fa fa-map hover:tw-text-indigo-400"></i></span>
+                    <span>Cities</span>
+                </div>
+                <i class="fa fa-angle-left"></i>
+            </a>
+            <a class="tw-p-2 tw-pl-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="">Students</a>
+            <a class="tw-p-2 tw-pl-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="">Alumnis</a>
+            <a class="tw-p-2 tw-pl-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="">Feedback</a>
+            <a class="tw-p-2 tw-pl-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="">Teams</a>
+            <a class="tw-p-2 tw-pl-4 hover:tw-text-indigo-400  tw-flex tw-justify-between" href="">News</a>
+        </div>
+    </div>
+</div>
+
+<script>
+
+
+</script>
