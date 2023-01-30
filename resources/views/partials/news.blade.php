cb8eb74492d5b5e9e3d9f9d92bd356383548d6ab
Najeebullah Mohammadi <najeebullah.m96@gmail.com>
2023-01-30 14:38:20 +0430

    news page recreation

--- resources/views/partials/news.blade.php
+++ resources/views/partials/news.blade.php
@@ -36,67 +36,91 @@
         </div>
 
         <div class="cards-group news-cards-group d-flex flex-wrap justify-content-around text-center">
-            <div class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
-                <img src="images/person_2.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+            <a href="#" class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
+                <div>
+                    <div class="card-img-wrap">
+                        <img src="images/person_2.jpeg" class="card-img-top" alt="...">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
-            </div>
-            <div class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
-                <img src="images/person_2.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </a>
+            <a href="#" class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
+                <div>
+                    <div class="card-img-wrap">
+                        <img src="images/person_2.jpeg" class="card-img-top" alt="...">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
-            </div>
-            <div class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
-                <img src="images/person_2.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </a>
+            <a href="#" class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
+                <div>
+                    <div class="card-img-wrap">
+                        <img src="images/person_2.jpeg" class="card-img-top" alt="...">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
-            </div>
-            <div class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
-                <img src="images/person_2.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </a>
+            <a href="#" class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
+                <div>
+                    <div class="card-img-wrap">
+                        <img src="images/person_2.jpeg" class="card-img-top" alt="...">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
-            </div>
-            <div class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
-                <img src="images/person_2.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </a>
+            <a href="#" class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
+                <div>
+                    <div class="card-img-wrap">
+                        <img src="images/person_2.jpeg" class="card-img-top" alt="...">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
-            </div>
-            <div class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
-                <img src="images/person_2.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </a>
+            <a href="#" class="card me-3 mb-3 col-sm-5 col-12 col-md-3">
+                <div>
+                    <div class="card-img-wrap">
+                        <img src="images/person_2.jpeg" class="card-img-top" alt="...">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
                 </div>
-            </div>
+            </a>
 
         </div>
+        <div class="text-center my-3">
+
+            <button class="btn btn-dark-blue text-center">load More</button>
+        </div>
+
+
+
+
+
     </div>
-    <div class="text-center my-3">
-
-        <button class="btn btn-dark-blue text-center">load More</button>
-    </div>
-
-
-
-
-
-</div>
 
 
 
