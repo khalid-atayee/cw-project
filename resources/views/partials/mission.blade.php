eb70dbd38eb1c2cd3837bbe79a107313e5e7500d
khalid-atayee <khalidatayee.76@gmail.com>
2023-02-06 02:10:12 +0430

    about us page refactored

--- resources/views/partials/mission.blade.php
+++ resources/views/partials/mission.blade.php
@@ -1,15 +1,15 @@
  <!-- mission start here =====
   ===== -->
 
- <div class="container " data-aos="fade-left">
+ <div class="container " id="mission-container" data-aos="fade-left">
    <row>
 
      <div class=" d-flex flex-column flex-md-row gap-3 align-items-start">
        <div class="col content">
-         <h1>
+         <h1 id="mission-title">
            Mission
          </h1>
-         <p class="text-left line-h-1">We help you find a job and we have one condition for that. Finish the Bootcamp successfully. It is intensive! We help you out with that. We help you find a job and we have one condition for that. Finish the Bootcamp successfully. It is intensive! We help you out with that. We help you find a job and we have one condition for that. Finish the Bootcamp successfully. It is intensive! We help you out with that.</p>
+         <p class="text-left line-h-1" id="mission-content-paragraph" >We help you find a job and we have one condition for that. Finish the Bootcamp successfully. It is intensive! We help you out with that. We help you find a job and we have one condition for that. Finish the Bootcamp successfully. It is intensive! We help you out with that. We help you find a job and we have one condition for that. Finish the Bootcamp successfully. It is intensive! We help you out with that.</p>
        </div>
 
        <div class="col mx-auto m-1">
--- resources/views/partials/vission.blade.php
+++ resources/views/partials/vission.blade.php
@@ -2,15 +2,15 @@
 ===== -->
 
 
- <div class="container " data-aos="fade-left">
+ <div class="container " id="vision-main-container" data-aos="fade-left">
    <row>
 
-     <div class=" d-flex flex-column flex-md-row-reverse gap-3 align-items-start mt-4">
+     <div class=" d-flex flex-column flex-md-row-reverse gap-3 align-items-start mt-4" >
        <div class="col content">
-         <h1>
+         <h1 id="vision-aboutUs-title">
            Vision
          </h1>
-         <p class="text-left line-h-1">We help you find a job and we have one condition for that. Finish the Bootcamp successfully.
+         <p  class="text-left line-h-1" id="vision-content-paragraph">We help you find a job and we have one condition for that. Finish the Bootcamp successfully.
            It is intensive! We help you out with that.We help you find a job and we have one condition for that. Finish
            the Bootcamp successfully.
            It is intensive! We help you out with that.We help you find a job and we have one condition for that. Finish
@@ -20,7 +20,7 @@
            It is intensive! We help you out with that.</p>
        </div>
 
-       <div class="col mx-auto m-1">
+       <div class="col mx-auto m-1" id="vision-img">
          <img src="images/vision.jpg" alt="" class="h-100 align-self-center" data-aos="fade-left">
        </div>
      </div>
